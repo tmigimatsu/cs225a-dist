c8bd27e8502cef0c674659f5882d5939e838119c
Toki Migimatsu <toki.migimatsu@gmail.com>
2017-05-19 10:29:15 -0700

    Adding lines for gcc-5 installation in 14.04

--- install.sh
+++ install.sh
@@ -6,6 +6,11 @@ set -e
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
 	sudo apt-get install curl cmake libeigen3-dev libtinyxml2-dev libjsoncpp-dev libhiredis-dev libglfw3-dev xorg-dev freeglut3-dev libasound2-dev libusb-1.0-0-dev redis-server
+	# Install gcc 5 for Ubuntu 14.04:
+	# sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+	# sudo apt-get update
+	# sudo apt-get install gcc-5 g++-5
+	# sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 60 --slave /usr/bin/g++ g++ /usr/bin/g++-5
 elif [[ "$OSTYPE" == "darwin"* ]]; then
 	brew install cmake eigen redis hiredis tinyxml2 jsoncpp glfw3
 fi
